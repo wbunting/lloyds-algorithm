07e1812057d9fd41a1e0dc0760b4a466dd2db7c0
wbunting <will.bunting@protonmail.com>
2024-04-30 12:45:20 +0900

    Correctly working lloyd algo -- Voronoi is too slow to do 1k pts @60fps

--- src/gui.c
+++ src/gui.c
@@ -9,9 +9,76 @@
 
 #define APP_PLUG
 
+#define CLAMP(x, min, max) ((x) < (min) ? (min) : ((x) > (max) ? (max) : (x)))
+
 bool IsCounterclockwise(Vector2 p1, Vector2 p2, Vector2 p3) {
   float area = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
-  return (area > 0); // Positive area means counterclockwise
+  return (area > 0);
+}
+
+float calculateArea(const Cell *cell) {
+  float area = 0.0f;
+  for (int i = 0, j = cell->num_vertices - 1; i < cell->num_vertices; j = i++) {
+    area += (cell->vertices[j].x + cell->vertices[i].x) *
+            (cell->vertices[j].y - cell->vertices[i].y);
+  }
+  return area / 2.0f;
+}
+
+Vector2 Vector2Add(Vector2 v1, Vector2 v2) {
+  Vector2 result = {v1.x + v2.x, v1.y + v2.y};
+
+  return result;
+}
+
+Vector2 Vector2Subtract(Vector2 v1, Vector2 v2) {
+  Vector2 result = {v1.x - v2.x, v1.y - v2.y};
+
+  return result;
+}
+
+Vector2 Vector2Scale(Vector2 v, float scale) {
+  Vector2 result = {v.x * scale, v.y * scale};
+
+  return result;
+}
+
+Vector2 Vector2Normalize(Vector2 v) {
+  Vector2 result = {0};
+  float length = sqrtf((v.x * v.x) + (v.y * v.y));
+
+  if (length > 0) {
+    float ilength = 1.0f / length;
+    result.x = v.x * ilength;
+    result.y = v.y * ilength;
+  }
+
+  return result;
+}
+
+Vector2 calculateCentroid(const Cell *cell) {
+  Vector2 centroid = {0.0f, 0.0f};
+  float A = calculateArea(cell);
+  float factor = 0.0f;
+
+  for (int i = 0, j = cell->num_vertices - 1; i < cell->num_vertices; j = i++) {
+    factor = (cell->vertices[i].x * cell->vertices[j].y -
+              cell->vertices[j].x * cell->vertices[i].y);
+    centroid.x += (cell->vertices[i].x + cell->vertices[j].x) * factor;
+    centroid.y += (cell->vertices[i].y + cell->vertices[j].y) * factor;
+  }
+
+  float sixArea = 6.0f * A;
+  centroid.x /= sixArea;
+  centroid.y /= sixArea;
+
+  // Correcting for polygons in CW order
+  // if (A < 0) {
+  //   centroid.x = -centroid.x;
+  //   centroid.y = -centroid.y;
+  // }
+
+  return centroid;
 }
 
 Vector2 midPoint(Vector2 a, Vector2 b) {
@@ -20,8 +87,8 @@ Vector2 midPoint(Vector2 a, Vector2 b) {
 }
 
 typedef struct {
-  Vector2 point;     // A point on the line
-  Vector2 direction; // Direction vector of the line
+  Vector2 point;
+  Vector2 direction;
 } Line;
 
 Line perpendicularBisector(Vector2 a, Vector2 b) {
@@ -35,46 +102,33 @@ Line perpendicularBisector(Vector2 a, Vector2 b) {
 Vector2 intersection(Segment seg, Line bisector, int *intersect) {
   Vector2 result = {0, 0};
 
-  // Extracting coordinates and direction components
   double x1 = seg.start.x, y1 = seg.start.y;
   double x2 = seg.end.x, y2 = seg.end.y;
   double px = bisector.point.x, py = bisector.point.y;
   double dx = bisector.direction.x, dy = bisector.direction.y;
 
-  // Calculating the parameters for the line equation of the segment
   double seg_dx = x2 - x1;
   double seg_dy = y2 - y1;
 
-  // Line equation for segment: (x - x1) / seg_dx = (y - y1) / seg_dy
-  // Line equation for bisector: x = px + t * dx, y = py + t * dy
-
-  // Determinant to solve for intersection
   double determinant = -seg_dx * dy + seg_dy * dx;
 
-  // If determinant is zero, lines are parallel
   if (fabs(determinant) < 1e-10) {
-    *intersect = 0; // No intersection
+    *intersect = 0;
     return result;
   }
 
-  // Solve linear system:
-  // (x1 + t * seg_dx) = px + s * dx
-  // (y1 + t * seg_dy) = py + s * dy
   double t = -1 * (dx * (y1 - py) - dy * (x1 - px)) / determinant;
-  // double s = (seg_dx * (py - y1) + seg_dy * (px - x1)) / determinant;
 
-  // Check if the intersection point t is within the bounds of the segment
   if (t >= 0 && t <= 1) {
     result.x = x1 + t * seg_dx;
     result.y = y1 + t * seg_dy;
-    // the intersection cannot be x2 y2 exactly
     if (result.x == x2 && result.y == y2) {
       *intersect = 0;
     } else {
-      *intersect = 1; // Intersection occurs
+      *intersect = 1;
     }
   } else {
-    *intersect = 0; // Intersection point is not within the segment
+    *intersect = 0;
   }
 
   return result;
@@ -83,41 +137,23 @@ Vector2 intersection(Segment seg, Line bisector, int *intersect) {
 APP_PLUG int plug_update(struct app_memory *Memory) {
   ASSERT(sizeof(struct app_state) <= Memory->PermanentStorageSize);
 
+  int screenWidth = GetScreenWidth();
+  int screenHeight = GetScreenHeight();
+
   struct app_state *AppState = (struct app_state *)Memory->PermanentStorage;
   if (!Memory->IsInitialized) {
-    float centerX = GetScreenWidth() / 2.0f;
-    float centerY = GetScreenHeight() / 2.0f;
+    float centerX = screenWidth / 2.0f;
+    float centerY = screenHeight / 2.0f;
 
-    AppState->num_vertices = 4;
-
-    AppState->vertices[0].position = (Vector2){100.000000, 300.000000};
-    AppState->vertices[0].color =
-        (Color){GetRandomValue(0, 255), GetRandomValue(0, 255),
-                GetRandomValue(0, 255), 255};
-    AppState->vertices[1].position = (Vector2){200.000000, 200.000000};
-    AppState->vertices[1].color =
-        (Color){GetRandomValue(0, 255), GetRandomValue(0, 255),
-                GetRandomValue(0, 255), 255};
-    AppState->vertices[2].position = (Vector2){300.000000, 400.000000};
-    AppState->vertices[2].color =
-        (Color){GetRandomValue(0, 255), GetRandomValue(0, 255),
-                GetRandomValue(0, 255), 255};
-    AppState->vertices[3].position = (Vector2){200.000000, 100.000000};
-    AppState->vertices[3].color =
-        (Color){GetRandomValue(0, 255), GetRandomValue(0, 255),
-                GetRandomValue(0, 255), 255};
+    AppState->num_vertices = 100;
 
     for (int i = 0; i < AppState->num_vertices; i++) {
-      // AppState->vertices[i].position =
-      //     (Vector2){centerX + (GetRandomValue(-centerX, centerX)),
-      //               centerY + (GetRandomValue(-centerY, centerY))};
-      AppState->vertices[i].velocity =
-          (Vector2){GetRandomValue(-1, 1), GetRandomValue(-1, 1)};
+      AppState->vertices[i].position = (Vector2){
+          centerX + GetRandomValue(-50, 50), centerY + GetRandomValue(-50, 50)};
+      AppState->vertices[i].color =
+          (Color){GetRandomValue(0, 255), GetRandomValue(0, 255),
+                  GetRandomValue(0, 255), 255};
       AppState->vertices[i].centroid = (Vector2){0};
-
-      // print the seed vertices
-      printf("Vertex %d: (%f, %f)\n", i, AppState->vertices[i].position.x,
-             AppState->vertices[i].position.y);
     }
 
     Memory->IsInitialized = 1;
@@ -125,60 +161,16 @@ APP_PLUG int plug_update(struct app_memory *Memory) {
     printf("Initialized app state\n");
   }
 
-  // get the min x coordinate among vertices
-  float min_x = AppState->vertices[0].position.x;
-  for (int i = 1; i < AppState->num_vertices; i++) {
-    if (AppState->vertices[i].position.x < min_x) {
-      min_x = AppState->vertices[i].position.x;
-    }
-  }
-
-  // get the max x coordinate among vertices
-  float max_x = AppState->vertices[0].position.x;
-  for (int i = 1; i < AppState->num_vertices; i++) {
-    if (AppState->vertices[i].position.x > max_x) {
-      max_x = AppState->vertices[i].position.x;
-    }
-  }
-
-  // get the min y coordinate among vertices
-  float min_y = AppState->vertices[0].position.y;
-  for (int i = 1; i < AppState->num_vertices; i++) {
-    if (AppState->vertices[i].position.y < min_y) {
-      min_y = AppState->vertices[i].position.y;
-    }
-  }
-
-  // get the max y coordinate among vertices
-  float max_y = AppState->vertices[0].position.y;
-  for (int i = 1; i < AppState->num_vertices; i++) {
-    if (AppState->vertices[i].position.y > max_y) {
-      max_y = AppState->vertices[i].position.y;
-    }
-  }
-
-  // padding is 1px for now. Box with min / max and padding
-  // Rectangle box = {
-  //     min_x - 50,
-  //     min_y - 50,
-  //     max_x - min_x + 100,
-  //     max_y - min_y + 100,
-  // };
-  //
   Rectangle box = {
       0,
       0,
-      GetScreenWidth(),
-      GetScreenHeight(),
+      screenWidth,
+      screenHeight,
   };
-
-  // Draw the box
-  DrawRectangleLinesEx(box, 1, RED);
 
   // Calculate Voronoi Diagram
   for (int i = 0; i < AppState->num_vertices; i++) {
     AppState->cells[i].num_vertices = 4;
-    // set the initial cell to be the bounding box including padding
     AppState->cells[i].vertices[0] = (Vector2){box.x, box.y};
     AppState->cells[i].vertices[1] = (Vector2){box.x + box.width, box.y};
     AppState->cells[i].vertices[2] =
@@ -191,12 +183,6 @@ APP_PLUG int plug_update(struct app_memory *Memory) {
 
       Line bisector = perpendicularBisector(AppState->vertices[i].position,
                                             AppState->vertices[j].position);
-
-      // Draw the bisector extended to the screen edges
-      Vector2 p1 = bisector.point;
-      Vector2 p2 = (Vector2){p1.x + bisector.direction.x * 1000,
-                             p1.y + bisector.direction.y * 1000};
-      DrawLineV(p1, p2, GREEN);
 
       // check for intersection of the bisector with the cell walls
       int num_intersections = 0;
@@ -262,7 +248,6 @@ APP_PLUG int plug_update(struct app_memory *Memory) {
           AppState->cells[i].vertices[k] = AppState->cells[i].temp_vertices[k];
         }
 
-        // reset the temp cell
         AppState->cells[i].temp_vertex_count = 0;
         AppState->cells[i].temp_vertices[0] = (Vector2){0};
       }
@@ -270,9 +255,16 @@ APP_PLUG int plug_update(struct app_memory *Memory) {
   }
 
   BeginDrawing();
-  ClearBackground(DARKGRAY);
+  ClearBackground(LIGHTGRAY);
 
   for (int i = 0; i < AppState->num_vertices; i++) {
+    float area = (-1.0f * 10 * calculateArea(&AppState->cells[i])) /
+                 (screenWidth * screenHeight);
+
+    float intensity = CLAMP(area, 0.1, 0.7);
+
+    Color color = (Color){255, 0, 0, (unsigned char)(intensity * 255)};
+
     for (int j = 0; j < AppState->cells[i].num_vertices; j++) {
       Vector2 p1 = AppState->vertices[i].position;
       Vector2 p2 = AppState->cells[i].vertices[j];
@@ -280,34 +272,30 @@ APP_PLUG int plug_update(struct app_memory *Memory) {
                        .vertices[(j + 1) % AppState->cells[i].num_vertices];
 
       if (!IsCounterclockwise(p1, p2, p3)) {
-        DrawTriangle(p1, p2, p3, AppState->vertices[i].color);
+        DrawTriangle(p1, p2, p3, color);
       } else {
-        DrawTriangle(p1, p3, p2, AppState->vertices[i].color);
+        DrawTriangle(p1, p3, p2, color);
       }
-      // DrawTriangleLines(p1, p2, p3, BLACK);
+      DrawLineV(AppState->cells[i].vertices[j],
+                AppState->cells[i]
+                    .vertices[(j + 1) % AppState->cells[i].num_vertices],
+                WHITE);
     }
   }
 
+  // UPDATE (LLoyd)
   for (int i = 0; i < AppState->num_vertices; i++) {
-    DrawCircleV(AppState->vertices[i].position, 2, WHITE);
-    // DrawText(TextFormat("%d", i), AppState->vertices[i].position.x + 4,
-    //          AppState->vertices[i].position.y + 4, 10, WHITE);
-  }
+    Vector2 centroid = calculateCentroid(&AppState->cells[i]);
+    // DrawCircleV(AppState->vertices[i].position, 2, WHITE);
+    // DrawCircleV(centroid, 2, BLACK);
 
-  // update the vertex positions
-  for (int i = 0; i < AppState->num_vertices; i++) {
-    AppState->vertices[i].position.x += AppState->vertices[i].velocity.x;
-    AppState->vertices[i].position.y += AppState->vertices[i].velocity.y;
-
-    // check for collision with the screen edges
-    if (AppState->vertices[i].position.x <= 0 ||
-        AppState->vertices[i].position.x >= GetScreenWidth()) {
-      AppState->vertices[i].velocity.x *= -1;
-    }
-    if (AppState->vertices[i].position.y <= 0 ||
-        AppState->vertices[i].position.y >= GetScreenHeight()) {
-      AppState->vertices[i].velocity.y *= -1;
-    }
+    Vector2 direction =
+        Vector2Subtract(centroid, AppState->vertices[i].position);
+    // Normalize the direction vector and scale it by the step factor
+    direction = Vector2Scale(Vector2Normalize(direction), 1.0f);
+    // Update the vertex position
+    AppState->vertices[i].position =
+        Vector2Add(AppState->vertices[i].position, direction);
   }
 
   DrawFPS(10, 10);
